0cd3777fddc0fc87ea4fe3fab061b37baab9cd52
luvemil <taran.marco@gmail.com>
2015-03-28 15:13:45 +0100

    Add a better docstring

--- notmuchData.py
+++ notmuchData.py
@@ -3,7 +3,10 @@ from notmuch import Database, Query
 import re
 
 class mailDir(Query):
-    """docstring for mailDir"""
+    """Works like Query:
+        db = Database(path)
+        addr = mailDir(db, pattern).search_addresses()
+    """
     def __init__(self, *args, **kwargs):
         super(mailDir, self).__init__(*args, **kwargs)
         self.regexp = re.compile(r".*<(.*)>")
@@ -15,7 +18,7 @@ class mailDir(Query):
 
     def search_addresses(self):
         """Returns an iterator containing the email of the sender of the nth
-        matching the string, as in search_messages(string)."""
+        matching the string, as in search_messages()."""
         msgs = self.search_messages()
         addresses = map(
             lambda x: self.regexp.sub(r'\1',x.get_header("from")),
